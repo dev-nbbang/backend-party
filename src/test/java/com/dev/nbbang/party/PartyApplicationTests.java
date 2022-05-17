dec87497b1bce1b61075998ca301cb6b26e8e2a4
zayson <jymaeng95@gmail.com>
2022-05-17 17:36:29 +0900

    init: 프로젝트 초기화

---
+++ src/test/java/com/dev/nbbang/party/PartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.nbbang.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
