940c892510dd3ba56cba1801b49855e5de1301f6
zayson <jymaeng95@gmail.com>
2022-05-28 11:05:14 +0900

    test: Party API Persistence Layer 테스트 - [#10]

--- src/test/java/com/dev/nbbang/party/domain/party/repository/PartyRepositoryTest.java
+++ src/test/java/com/dev/nbbang/party/domain/party/repository/PartyRepositoryTest.java
@@ -2,7 +2,6 @@ package com.dev.nbbang.party.domain.party.repository;
 
 import com.dev.nbbang.party.domain.ott.entity.Ott;
 import com.dev.nbbang.party.domain.party.entity.Party;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +11,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
